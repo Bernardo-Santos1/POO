0d880c582a8700530ee55543eaaeb6edd79d9276
BERNARDO DOS SANTOS DE SANTANA <Bernardo301204.rj@gmail.com>
2026-08-30 09:55:51 -0300

    first commit

--- Fracao/src/Fracao.java
+++ Fracao/src/Fracao.java
@@ -25,8 +25,8 @@ public class Fracao {
     public Fracao somar(Fracao outra) {
 
 
-        int numResultado = 1;  // ToDo
-        int denomResultado = 1;  // ToDo
+        int numResultado = 1;
+        int denomResultado = 1;
 
         int num = getNumerador();
         int den = getDenominador();
@@ -58,21 +58,73 @@ public class Fracao {
         double resp = a*(numerador*1.0)/(denominador*1.0);
 
         return resp;
-
-          // ToDo IMPLEMENT ME!!!
     }
 
     public Fracao getFracaoIrredutivel() {
-        int a = Aritmetica.mdc();
-        return null;  // ToDo IMPLEMENT ME!!!
+        int a = Aritmetica.mdc(getNumerador(), getDenominador());
+
+        int numerador = getNumerador()/a;
+        int denominador = getDenominador()/a;
+
+        int sinal;
+
+        if(getSinal()){
+            sinal = 1;
+        }else{
+            sinal =-1;
+        }
+
+        numerador *= sinal;
+
+        Fracao irredutivel = new Fracao(numerador, denominador);
+
+        return irredutivel;
     }
 
     public Fracao multiplicar(Fracao outra) {
-        return null;  // ToDo IMPLEMENT ME!!!
+        int num1 = getNumerador();
+        int denom1 = getDenominador();
+
+        int num2 = outra.getNumerador();
+        int denom2 = outra.getDenominador();
+
+        int sinal = 1;
+
+        if (!getSinal() || !outra.getSinal()){
+            sinal = -1;
+        }
+
+        int finalNum = num1 * num2 * sinal;
+        int finalDenom = denom1 * denom2;
+
+        Fracao multiplicada = new Fracao(finalNum, finalDenom);
+
+
+        return multiplicada;
     }
 
     @Override
     public String toString() {
-        return null;  // ToDo IMPLEMENT ME!!!
+        int numerador = getNumerador();
+        int denominador = getDenominador();
+        String resp = "";
+
+        if(numerador == 0){
+            resp = "0";
+            return resp;
+        }
+
+        if(!getSinal()){
+            resp = "-";
+        }
+
+        if (denominador == 1){
+            resp += numerador;
+            return resp;
+        }
+
+        resp += numerador + "/" + denominador;
+
+        return resp;
     }
 }
